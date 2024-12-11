dbc8d978a8bda5efcb4af370557bfc1f33c641f6
Fábio Filho <145404540+FabinhoFarias@users.noreply.github.com>
2024-12-11 16:51:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,24 +15,24 @@ def QuebraComponentesQuestao(QuestaoJson, TextoQuestao, contador):
         ListaComponentesQuestao.append(TextoQuestao)
         return ListaComponentesQuestao
     else:
-        ListaPivo = TextoQuestao.split(f"![]({QuestaoJson['files'][contador]})")
+        ListaPivo = TextoQuestao.split(f"![]({QuestaoJson["files"][contador]})")
         ListaComponentesQuestao.append(ListaPivo[0])
         ListaComponentesQuestao.append(QuestaoJson["files"][contador])
         contador += 1
         if ListaPivo[-1]:
             return QuebraComponentesQuestao(QuestaoJson, ListaPivo[-1], contador)
-        return ListaComponentesQuestao      
 
 def ImprimirQuestao(QUESTAO, ListaComponentesQuestao, DOMINIO):
     for Componente in ListaComponentesQuestao:
         if DOMINIO in Componente:
             st.image(Componente, caption="Imagem da Questão")
         else:
-            st.html(f"<br><p>{Componente}<br></p>")
-        #Implementar o LaTeX depois
-    st.write(f"**{QUESTAO['alternativesIntroduction']}**") #https://api.enem.dev/v1/exams/2011/questions/141 fazer gambiarra depois para poder evitar questoes incompletas como essa
+            st.write(Componente)
+    st.markdown("---")
+    st.write(f"**{QUESTAO['alternativesIntroduction']}**") # https://api.enem.dev/v1/exams/2011/questions/141 fazer gambiarra depois para poder evitar questoes incompletas como essa
     for Alternativa in QUESTAO["alternatives"]:
         # crio botão
+        st.markdown("---")
         if st.button(f"Alternativa {Alternativa['letter']}"):
             if Alternativa["isCorrect"]:
                 st.write(f"Alternativa CORRETA :)")
@@ -42,7 +42,7 @@ def ImprimirQuestao(QUESTAO, ListaComponentesQuestao, DOMINIO):
             st.write(f"{Alternativa["text"]}")
         else: # há imagem
             st.image(Alternativa["file"], caption="Alternativa")
-
+    st.markdown("---")
 
 
     # st.html(f"{Questao["context"]}")
@@ -57,19 +57,11 @@ def main():
     if "page" not in st.session_state:
         st.session_state.page = "PaginaInicial"
 
-    def NavegarEntrePaginas(pagina):
-        st.session_state.page = pagina
-
     if st.session_state.page == "PaginaInicial":
-        st.title("Sistema de Navegação no Streamlit")
-        st.header("Selecione uma opção:")
-        st.button("Ir para Página de Conteúdos", use_container_width=True, on_click=NavegarEntrePaginas, args=("PaginaDeConteudos"))
-        st.button("Ir para Página de Listas", use_container_width=True, on_click=NavegarEntrePaginas, args=("PaginaDeListas"))
-
-    elif st.session_state.page == "PaginaDeListas":
-        pooo()  # Função do arquivo 'PaginaDeListas'
+        st.title("Procure a questão que você deseja !")
+        
     AnoQuestao    = int(st.slider("Digite o Ano da Prova:", min_value=2009, max_value=2023))    
-    NumeroQuestao = int(st.slider("Digite uma questão:"   , min_value=1, max_value=180))  
+    NumeroQuestao = int(st.slider("Digite uma questão:"   , min_value=1,    max_value=180))  
     
     Dominio = "https://api.enem.dev"
     Questao = RequestTextoQuestao(AnoQuestao, NumeroQuestao)
